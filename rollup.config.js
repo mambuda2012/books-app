cc0c9b1baa47f756dd3a66e1abaa4ecc34a87562
Mazmanayn Yura <mambuda2012@gmail.com>
2025-12-24 22:51:11 +0300

    Базовая настройка проекта

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import nodeResolve from '@rollup/plugin-node-resolve'
+import css from "rollup-plugin-import-css"
+
+export default {
+    input: "src/app.js",
+    output: { dir: "dist", format: "iife" },
+    plugins: [ css({
+			output:'bundle.css'
+		}),nodeResolve() ]
+};
